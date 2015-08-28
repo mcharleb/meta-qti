1aa9fe60a5de3236b06ad928d03bda8b4f4e5bf5
Gustavo Solaira <gustavos@qti.qualcomm.com>
2015-08-28 10:34:59 -0700

    meta-qti: Enable reboot2fastboot in other modes
    Modify reboot2fastboot to allow mode to be set via the
    command line.
    
    Change-Id: Ib70439600df2002e6a6907696a2afaa753300b3c

--- recipes-bsp/reboot2fastboot/files/reboot2fastboot.c
+++ recipes-bsp/reboot2fastboot/files/reboot2fastboot.c
@@ -7,18 +7,55 @@
 #include <linux/reboot.h>
 #include <stdio.h>
 #include <errno.h>
+#include <unistd.h>
+
+void print_usage(char *command) {
+    printf("Usage:\n%s <boot_mode>\n", command);
+    printf("List of boot modes:\n");
+    printf("bootloader: reboot to fastboot mode\n");
+    printf("recovery: reboot to recovery mode\n");
+    printf("edl: reboot to emergency download mode\n");
+}
 
 /*****************************************************************************
 * Reboot device and wait for fastboot processing
-* Invokes reboot systemcall with parameter "bootloader"
+* Invokes reboot systemcall with string option passed from the command line
+* Options available are:
+* - bootloader: Reboots to fastboot mode (Default)
+* - edl: Reboots to emergency download mode for firmware programming
+* - recovery: Reboots to recovery mode for applying system updates
 *****************************************************************************/
-int main(void)
+int main(int argc, char *argv[])
 {
 	int ret = -1;
-	printf("Rebooting to fastboot mode..\n");
+    int c;
+    char *mode = NULL;
+    char *bootloader = "bootloader";
+
+    while ((c = getopt (argc, argv, "h")) != -1) {
+        switch (c) {
+            case 'h':
+                print_usage(argv[0]);
+                return 0;
+                break;
+        }
+    }
+
+	if (argc < 2) {
+		mode = bootloader;
+	} else if (strcmp(argv[1], "bootloader") == 0 ||
+               strcmp(argv[1], "recovery") == 0 ||
+               strcmp(argv[1], "edl") == 0) {
+        mode = argv[1];
+	} else {
+        print_usage(argv[0]);
+        return -EINVAL;
+    }
+
+	printf("Rebooting to %s mode..\n", mode);
 	sleep(1);
 	ret = __rfastboot(LINUX_REBOOT_MAGIC1, LINUX_REBOOT_MAGIC2,
-               			LINUX_REBOOT_CMD_RESTART2, "bootloader");
+			  LINUX_REBOOT_CMD_RESTART2, mode);
 
 	if(ret)
 		printf("ERROR: Reboot failed errno(%d)\n", errno);
