e23c023def1b0325db612b904cfbc351668a4f4a
Gustavo Solaira <gustavos@qti.qualcomm.com>
2015-10-14 19:10:15 -0700

    ATL-4104: Remove sleep wait from adsp services
    ADSP init script will emit "adsp" command for upstart when the
    subsystem is up. It queries this through sysfs.
    
    Change-Id: Iba06997d515980b88498d34fb379ee598c10c82d

--- recipes-extended/q6-admin/files/q6-admin.sh
+++ recipes-extended/q6-admin/files/q6-admin.sh
@@ -34,3 +34,9 @@ echo 1 > /sys/module/subsystem_restart/parameters/enable_debug
 # Bring adsp out of reset
 echo "[INFO] Bringing ADSP out of reset"
 echo 1 > /sys/kernel/boot_adsp/boot
+
+# Don't leave until ADSP is up
+watch -n 1 --precise -g grep -m 1 "2" /sys/kernel/debug/msm_subsys/adsp && true
+
+# Emit adsp
+initctl emit adsp
