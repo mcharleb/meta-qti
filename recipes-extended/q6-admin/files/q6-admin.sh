1bf283379902543839c0a0cd7c044ed4f184091a
Dhruv Kumar Chawda <c_dchawd@quicinc.com>
2015-11-23 16:17:37 -0800

    Remove the adsp firmware symlink
    Change-Id: I51263e63381f465349ed2794707490616e3f4779

--- recipes-extended/q6-admin/files/q6-admin.sh
+++ recipes-extended/q6-admin/files/q6-admin.sh
@@ -8,27 +8,6 @@
 #
 ###############################################################################
 
-echo "[INFO] Mounting the modem partition"
-mkdir -p /firmware
-mount -t vfat /dev/mmcblk0p1 /firmware
-cd /firmware/image/
-fwfiles=$(ls adsp*)
-
-# Create the links if needed
-echo "[INFO] Creating links to /lib/firmware"
-cd /lib/firmware
-for fwfile in $fwfiles; do
-        fw_file=$(ls $fwfile)
-        if ("$fw_file"=="$fwfile"); then
-                continue
-        else
-                cd /firmware/image
-                for imgfile in adsp*; do
-                    ln -s /firmware/image/$imgfile /lib/firmware/$imgfile 2>/dev/null
-                done
-        fi
-done
-
 # FIXME: See ATL-3054
 echo 1 > /sys/module/subsystem_restart/parameters/enable_debug
 # Bring adsp out of reset
